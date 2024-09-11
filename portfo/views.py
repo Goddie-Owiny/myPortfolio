dc9a563bfc7ddd6ef0eb93dfdd0dc9e0fdddca23
Goddie-Owiny <goddiegodfreyz07@gmail.com>
2024-09-11 23:45:42 +0300

    changed the navbar

--- portfo/views.py
+++ portfo/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Blog
+from . forms import *
 
 # Create your views here.
 def index(request):
     return render(request, 'portfo/index.html')
 
 def contact(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('contact')
     return render(request, 'portfo/contact.html')
 
 def about(request):
